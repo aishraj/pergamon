45e5b3d0968f59fc1b488f0bc5607296db29d59b
ge3k <ge3k@users.noreply.github.com>
2014-04-11 07:57:45 +0530

    Initial Commit

---
+++ src/main/java/UnionFind.java
@@ -0,0 +1,57 @@
+/**
+ * Created by ge3k on 2/4/14.
+ */
+public class UnionFind {
+
+    int[] parentArray;
+    int[] rank;
+    int count;
+
+    public UnionFind(int N) {
+        if (N < 0) throw new IllegalArgumentException();
+        this.count = N;
+        parentArray = new int[N];
+        rank = new int[N];
+        for (int i = 0; i < N; i++) {
+            parentArray[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public void union(int p, int q) {
+        if (connected(p,q)) {
+            return;
+        }
+        if (rank[p] > rank[q]) {
+            parentArray[q] = p;
+        }
+        else if ( rank[q] > rank[p]) {
+            parentArray[p] = q;
+        }
+        else {
+            parentArray[q] = p;
+            rank[p] += rank[q];
+            count--;
+        }
+    }
+
+    public int find(int p) {
+        if (p < 0 || p > parentArray.length) throw new IndexOutOfBoundsException();
+        while ( p != parentArray[p]) {
+            parentArray[p] = parentArray[parentArray[p]];
+            p = parentArray[p];
+        }
+        return p;
+    }
+
+    public boolean connected(int p, int q) {
+        if (find(p) == find(q)) {
+            return true;
+        }
+        return false;
+    }
+
+    public int count() {
+        return count;
+    }
+}
---
+++ src/test/java/GroovyUnionFindTest.groovy
@@ -0,0 +1,20 @@
+/**
+ * Created by ge3k on 2/4/14.
+ */
+class GroovyUnionFindTest extends GroovyTestCase {
+
+
+    void testConnected() {
+        UnionFind unionFind = new UnionFind(10)
+        unionFind.union(4,3)
+        unionFind.union(3,8)
+        unionFind.union(6,5)
+        unionFind.union(9,4)
+        unionFind.union(2,1)
+        unionFind.union(5,0)
+        unionFind.union(7,2)
+        unionFind.union(6,1)
+        assertEquals unionFind.count(),2
+    }
+
+}
